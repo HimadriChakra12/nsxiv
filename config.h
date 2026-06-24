3c04ca5962c8a81f9ec8e1193277b2ef79ba2a28
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-06-24 08:29:18 +0600

    clean config.h

--- config.h
+++ config.h
@@ -17,37 +17,19 @@ static const bool TOP_STATUSBAR = false;
 #endif
 #ifdef INCLUDE_IMAGE_CONFIG
 
-/* levels (in percent) to use when zooming via '-' and '+':
- * (first/last value is used as min/max zoom level)
- */
 static const float zoom_levels[] = {
 	 12.5,  25.0,  50.0,  75.0,
 	100.0, 150.0, 200.0, 400.0, 800.0
 };
 
-/* default slideshow delay (in sec, overwritten via -S option): */
 static const int SLIDESHOW_DELAY = 5;
 
-/* color correction: the user-visible ranges [-CC_STEPS, 0] and
- * (0, CC_STEPS] are mapped to the ranges [0, 1], and (1, *_MAX].
- * Higher step count will have higher granulairy.
- */
 static const int    CC_STEPS        = 32;
 static const double GAMMA_MAX       = 10.0;
 static const double BRIGHTNESS_MAX  = 2.0;
 static const double CONTRAST_MAX    = 4.0;
-
-/* command i_scroll pans image 1/PAN_FRACTION of screen width/height */
 static const int PAN_FRACTION = 5;
 
-/* percentage of memory to use for imlib2's cache size.
- *   3 means use 3% of total memory which is about 245MiB on 8GiB machine.
- *   0 or less means disable cache.
- * 100 means use all available memory (but not above CACHE_SIZE_LIMIT).
- *
- * NOTE: higher cache size means better image reloading performance, but also
- * higher memory usage.
- */
 static const int CACHE_SIZE_MEM_PERCENTAGE = 3;          /* use 3% of total memory for cache */
 static const int CACHE_SIZE_LIMIT = 256 * 1024 * 1024;   /* but not above 256MiB */
 static const int CACHE_SIZE_FALLBACK = 32 * 1024 * 1024; /* fallback to 32MiB if we can't determine total memory */
@@ -55,43 +37,22 @@ static const int CACHE_SIZE_FALLBACK = 32 * 1024 * 1024; /* fallback to 32MiB if
 #endif
 #ifdef INCLUDE_OPTIONS_CONFIG
 
-/* if false, pixelate images at zoom level != 100%,
- * toggled with 'a' key binding (overwritten via `--anti-alias` option)
- */
 static const bool ANTI_ALIAS = true;
-
-/* if true, use a checkerboard background for alpha layer,
- * toggled with 'A' key binding (overwritten via `--alpha-layer` option)
- */
 static const bool ALPHA_LAYER = false;
-
-/* list of whitelisted/blacklisted directory for thumbnail cache
- * (overwritten via --cache-{allow,deny} option).
- * see THUMBNAIL CACHING section in nsxiv(1) manpage for more details.
- */
 static const char TNS_FILTERS[] = "";
-/* set to true to treat `TNS_FILTERS` as a blacklist instead */
 static const bool TNS_FILTERS_IS_BLACKLIST = false;
 
 #endif
 #ifdef INCLUDE_THUMBS_CONFIG
 
-/* thumbnail sizes in pixels (width == height): */
 static const int thumb_sizes[] = { 32, 64, 96, 128, 160 };
-
-/* thumbnail size at startup, index into thumb_sizes[]: */
 static const int THUMB_SIZE = 3;
 
 #endif
 #ifdef INCLUDE_MAPPINGS_CONFIG
 
-/* these modifiers will be used when processing keybindings */
 static const unsigned int USED_MODMASK = ShiftMask | ControlMask | Mod1Mask;
-
-/* abort the keyhandler */
 static const KeySym KEYHANDLER_ABORT = XK_Escape;
-
-/* keyboard mappings for image and thumbnail mode: */
 static const keymap_t keys[] = {
 	/* modifiers    key               function              argument */
 	{ 0,            XK_q,             g_quit,               0 },
@@ -205,12 +166,8 @@ static const button_t buttons_tns[] = {
 	{ ControlMask,  5,                g_scroll_screen,      DIR_DOWN },
 };
 
-/* true means NAV_WIDTH is relative (33%), false means absolute (33 pixels) */
 static const bool NAV_IS_REL = true;
-/* width of navigation area, 0 disables cursor navigation, */
 static const unsigned int NAV_WIDTH = 33;
-
-/* mouse cursor on left, middle and right part of the window */
 static const cursor_t imgcursor[3] = {
 	CURSOR_LEFT, CURSOR_ARROW, CURSOR_RIGHT
 };
